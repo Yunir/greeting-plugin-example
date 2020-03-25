bfd07a929042b5d7d0c2ebb8b2d29ad06c81de50
Yunir <yunir.me@gmail.com>
2020-03-25 21:28:36 +0300

    modify logic of task

--- src/main/java/org/example/greeting/GreetingPlugin.java
+++ src/main/java/org/example/greeting/GreetingPlugin.java
@@ -5,9 +5,11 @@ import org.gradle.api.Project;
 
 public class GreetingPlugin implements Plugin<Project> {
     public void apply(Project project) {
-        project.getTasks().create("hello", Greeting.class, (task) -> { // <1>
+        project.getTasks().create("NEW_TASK_TO_USE", Greeting.class, (task) -> { // <1>
+            task.setGroup("Greet");
             task.setMessage("Hello");
             task.setRecipient("World");                                // <2>
+            task.sayGreeting();
         });
     }
 }
